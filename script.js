cd88aaf37697d862a12f7e5fe063d712947570a8
AsherCode <alonsosandovalch@gmail.com>
2021-12-24 11:21:04 -0500

    modified countdown

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const christmas = "Sat Dec 25 2021 12:00:00";
+const christmas = "25 Dec 2021" ;
 
 const secondsEl = document.getElementById('seconds');
 const minsEl = document.getElementById('mins');
@@ -10,17 +10,18 @@ function countdown() {
     const christmasDate = new Date(christmas);
     const currentDate = new Date();
 
-    const diff = new Date(christmasDate - currentDate);
-    const seconds = diff.getSeconds();
-    const minutes = diff.getMinutes();
-    const hours = diff.getHours();
-    const days = diff.getDate();
+    const diff = new Date(christmasDate - currentDate) / 1000;
+
+    const days = Math.floor(diff / 3600 / 24);
+    const hours = Math.floor(diff / 3600) % 24;
+    const mins = Math.floor(diff / 60) % 60;
+    const seconds = Math.floor(diff) % 60;
     
     daysEl.innerHTML = days;
     
     hoursEl.innerHTML = formatTime(hours);
     
-    minsEl.innerHTML = formatTime(minutes);
+    minsEl.innerHTML = formatTime(mins);
     
     secondsEl.innerHTML = formatTime(seconds);
 }
